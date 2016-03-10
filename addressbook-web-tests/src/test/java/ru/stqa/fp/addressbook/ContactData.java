f288b567af4dd4507a2f40356b4951d4fec93274
marat <marat3132@gmail.com>
2016-03-10 22:41:17 +0300

    UserCreationTest added

---
+++ addressbook-web-tests/src/test/java/ru/stqa/fp/addressbook/ContactData.java
@@ -0,0 +1,55 @@
+package ru.stqa.fp.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String middlename;
+    private final String lastname;
+    private final String nickname;
+    private final String title;
+    private final String company;
+    private final String address;
+    private final String phonehome;
+
+    public ContactData(String firstname, String middlename, String lastname, String nickname, String title, String company, String address, String phonehome) {
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.title = title;
+        this.company = company;
+        this.address = address;
+        this.phonehome = phonehome;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhonehome() {
+        return phonehome;
+    }
+}
